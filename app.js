1d55c6716c6d60d7dd77b9a271509fb9c05c8938
Marcelo Dapper <marcelo.dapper@gmail.com>
2016-10-16 12:09:41 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+
+app.use(function(req, res, next) {
+  next();
+});
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log("Express server is listening port", port);
+});
